a7bffc8912bed9890e0b494505acf8952cc6dbcb
Fatih Kürekçi <fatihkr02@gmail.com>
2024-12-12 19:41:47 +0300

    completed heading processes in writerpanel

---
+++ MvcProjeKampi/Controllers/WriterPanelController.cs
@@ -0,0 +1,74 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class WriterPanelController : Controller
+    {
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+
+        public ActionResult WriterProfile()
+        {
+            return View();
+        }
+
+        public ActionResult MyHeading()
+        {
+            var values = hm.GetListByWriter();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult NewHeading()
+        {
+            List<SelectListItem> categories = (from x in cm.GetList()
+                                               select new SelectListItem
+                                               {
+                                                   Text = x.CategoryName,
+                                                   Value = x.CategoryId.ToString()
+                                               }).ToList();
+            ViewBag.Categories = categories;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Now;
+            p.WriterId = 4;
+            p.HeadingStatus = true;
+            hm.HeadingAdd(p);
+            return RedirectToAction("MyHeading");
+        }
+
+        [HttpGet]
+        public ActionResult EditHeading(int id)
+        {
+            List<SelectListItem> categories = (from x in cm.GetList()
+                                               select new SelectListItem
+                                               {
+                                                   Text = x.CategoryName,
+                                                   Value = x.CategoryId.ToString()
+                                               }).ToList();
+            ViewBag.Categories = categories;
+
+            var headingValue = hm.GetById(id);
+            return View(headingValue);
+        }
+
+        [HttpPost]
+        public ActionResult EditHeading(Heading p)
+        {
+            hm.HeadingUpdate(p);
+            return RedirectToAction("MyHeading");
+        }
+    }
+}
