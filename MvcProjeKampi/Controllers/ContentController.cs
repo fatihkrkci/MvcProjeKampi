5c4d0a03b343000f1fb11c46e21106f1aed8219c
Fatih Kürekçi <fatihkr02@gmail.com>
2024-12-14 01:02:07 +0300

    project completed

--- MvcProjeKampi/Controllers/ContentController.cs
+++ MvcProjeKampi/Controllers/ContentController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,12 @@ namespace MvcProjeKampi.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetAllContent(string p)
+        {
+            var values = cm.GetList(p);
+            return View(values);
         }
 
         public ActionResult ContentByHeading(int id)
